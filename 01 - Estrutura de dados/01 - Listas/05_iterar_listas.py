a2101d7396a285ce1ea1a0b7a3f5fadc8f289a28
andrecauss <andrecauss@gmail.com>
2023-09-11 17:26:29 -0300

    código comentado

--- 01 - Estrutura de dados/01 - Listas/05_iterar_listas.py
+++ 01 - Estrutura de dados/01 - Listas/05_iterar_listas.py
@@ -1,8 +1,9 @@
 carros = ["gol", "celta", "palio"]
 
+# corre a lista normal trazendo cada elemento
 for carro in carros:
     print(carro)
 
-
+# corre a lista trazendo dois elementos, o índice e o elemento carro
 for indice, carro in enumerate(carros):
     print(f"{indice}: {carro}")
